8629e09fef7efb8a1087d13c7a190eda475efa68
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-10-12 14:43:51 +0200

    implement pretty-printer

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Write;
+
 use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub parser);
@@ -9,12 +11,50 @@ pub enum Bound<'a> {
     Lifetime(&'a str),
     Item(Item<'a>),
     Reference {
-        amp: &'a str,
         lifetime: &'a str,
-        mut_: Option<&'a str>,
+        mut_: bool,
         item: Item<'a>,
     },
     Tuple(Vec<Bound<'a>>),
+}
+
+impl<'a> Bound<'a> {
+    fn pretty_internal<Writer: Write>(
+        &self,
+        indent_level: usize,
+        writer: &mut Writer,
+    ) -> Result<(), std::fmt::Error> {
+        indent(indent_level, writer)?;
+        match self {
+            Self::Lifetime(lifetime) => writer.write_str(lifetime),
+            Self::Item(item) => item.pretty_internal(indent_level, writer),
+            Self::Reference {
+                lifetime,
+                mut_,
+                item,
+            } => {
+                writer.write_char('&')?;
+                writer.write_str(lifetime)?;
+                if *mut_ {
+                    writer.write_str(" mut")?;
+                }
+                writer.write_char(' ')?;
+                item.pretty_internal(indent_level, writer)
+            }
+            Self::Tuple(bounds) => {
+                writer.write_str("(\n")?;
+                for (idx, bound) in bounds.iter().enumerate() {
+                    bound.pretty_internal(indent_level + 1, writer)?;
+                    if idx != bounds.len() - 1 {
+                        writer.write_char(',')?;
+                    }
+                    writer.write_char('\n')?;
+                }
+                indent(indent_level, writer)?;
+                writer.write_char(')')
+            }
+        }
+    }
 }
 
 /// A struct, trait, enum, or typedef.
@@ -24,4 +64,53 @@ pub enum Bound<'a> {
 pub struct Item<'a> {
     name: Vec<&'a str>,
     generic_bounds: Vec<Bound<'a>>,
+}
+
+impl<'a> Item<'a> {
+    /// Pretty-print this Item into a new string.
+    pub fn pretty(&self) -> String {
+        let mut writer = String::new();
+        self.pretty_internal(0, &mut writer)
+            .expect("writing to String should never fail");
+        writer
+    }
+
+    /// Pretty-print this Item to the supplied writer.
+    pub fn pretty_to<Writer: Write>(&self, writer: &mut Writer) -> Result<(), std::fmt::Error> {
+        self.pretty_internal(0, writer)
+    }
+
+    fn pretty_internal<Writer: Write>(
+        &self,
+        indent_level: usize,
+        writer: &mut Writer,
+    ) -> Result<(), std::fmt::Error> {
+        for (idx, component) in self.name.iter().enumerate().rev() {
+            writer.write_str(component)?;
+            if idx != 0 {
+                writer.write_str("::")?;
+            }
+        }
+        if !self.generic_bounds.is_empty() {
+            writer.write_str("<\n")?;
+            for (idx, bound) in self.generic_bounds.iter().enumerate() {
+                bound.pretty_internal(indent_level + 1, writer)?;
+                if idx != self.generic_bounds.len() - 1 {
+                    writer.write_char(',')?;
+                }
+                writer.write_char('\n')?;
+            }
+            indent(indent_level, writer)?;
+            writer.write_char('>')?;
+        }
+        Ok(())
+    }
+}
+
+const INDENT: &str = "  ";
+fn indent<Writer: Write>(indent_level: usize, writer: &mut Writer) -> Result<(), std::fmt::Error> {
+    for _ in 0..indent_level {
+        writer.write_str(INDENT)?;
+    }
+    Ok(())
 }
--- src/parser_tests.rs
+++ src/parser_tests.rs
@@ -2,57 +2,59 @@ use super::parser::*;
 
 use rstest::rstest;
 
-#[rstest(input,
+#[rstest(
+    input,
     case("Arc"),
     case("sc_client_db::Backend"),
-    case("sc_service::client::Client"),
+    case("sc_service::client::Client")
 )]
 fn parse_name(input: &str) {
     let result = ItemParser::new().parse(input);
     println!("{:#?}", result);
     assert!(result.is_ok());
+    println!("{}", result.unwrap().pretty());
 }
 
-#[rstest(input,
+#[rstest(
+    input,
     case("Token"),
     case("Arc<Foo>"),
     case("Bar<Bat, Baz>"),
     case("super::sub::Thing<Asd, fgh::Jkl>"),
-    case("Level1<Level2<Level3<level4::Thing, level4::Other>>>"),
+    case("Level1<Level2<Level3<level4::Thing, level4::Other>>>")
 )]
 fn parse_item(input: &str) {
     let result = ItemParser::new().parse(input);
     println!("{:#?}", result);
     assert!(result.is_ok());
+    println!("{}", result.unwrap().pretty());
 }
 
-#[rstest(input,
+#[rstest(
+    input,
     case("Item<'a>"),
     case("Token<'input>"),
-    case("super::sub::Thing<Asd<'gfd>, fgh::Jkl<'zxc>>"),
+    case("super::sub::Thing<Asd<'gfd>, fgh::Jkl<'zxc>>")
 )]
 fn parse_item_with_lifetime(input: &str) {
     let result = ItemParser::new().parse(input);
     println!("{:#?}", result);
     assert!(result.is_ok());
+    println!("{}", result.unwrap().pretty());
 }
 
-#[rstest(input,
-    case("Vec<&'a str>"),
-    case("Arc<Mutex<&'a mut Foo>>"),
-)]
+#[rstest(input, case("Vec<&'a str>"), case("Arc<Mutex<&'a mut Foo>>"))]
 fn parse_item_with_borrowed_reference(input: &str) {
     let result = ItemParser::new().parse(input);
     println!("{:#?}", result);
     assert!(result.is_ok());
+    println!("{}", result.unwrap().pretty());
 }
 
-#[rstest(input,
-    case("Box<(Foo, Bar)>"),
-    case("Vec<(Item<'a>, &'static str)>"),
-)]
+#[rstest(input, case("Box<(Foo, Bar)>"), case("Vec<(Item<'a>, &'static str)>"))]
 fn parse_item_tuple(input: &str) {
     let result = ItemParser::new().parse(input);
     println!("{:#?}", result);
     assert!(result.is_ok());
+    println!("{}", result.unwrap().pretty());
 }
